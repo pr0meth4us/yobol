b180498e9feb6a0edbb6c7a4c6c89d33d9ebb732
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-08-06 12:25:48 +0700

    feat: dockerize

---
+++ backend/server.js
@@ -0,0 +1,6 @@
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
